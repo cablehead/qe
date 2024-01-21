f012816b10a627fd9aebd0b9728a74be8cfd50bc
Andy Gayton <andy@thecablelounge.com>
2024-01-20 21:11:57 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use wry::{
+    application::{
+        event::{Event, StartCause, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::WindowBuilder,
+    },
+    webview::WebViewBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let _webview = WebViewBuilder::new(window)
+        .unwrap()
+        .with_html("<html><body><h1>Hello World</h1></body></html>")
+        .unwrap()
+        .build()
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            Event::NewEvents(StartCause::Init) => println!("Wry application started!"),
+            _ => (),
+        }
+    });
 }
