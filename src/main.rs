2e4e341a382fedb8b0dcbb815e6b599b65a92161
Andy Gayton <andy@thecablelounge.com>
2024-01-20 21:20:58 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,28 @@ use wry::{
     webview::WebViewBuilder,
 };
 
-fn main() {
+fn main() -> Result<(), wry::Error> {
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Hello World")
+        .build(&event_loop)?;
 
     let _webview = WebViewBuilder::new(window)
-        .unwrap()
-        .with_html("<html><body><h1>Hello World</h1></body></html>")
-        .unwrap()
-        .build()
-        .unwrap();
+        .with_url("https://tauri.app")?
+        .build()?;
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
+            Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
             } => *control_flow = ControlFlow::Exit,
-            Event::NewEvents(StartCause::Init) => println!("Wry application started!"),
             _ => (),
         }
     });
+
+    Ok(())
 }
