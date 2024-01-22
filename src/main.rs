7001027bb3839c20524210f44d55e19ed5dfa4d5
Andy Gayton <andy@thecablelounge.com>
2024-01-22 10:25:24 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, Read};
+
+use base64::{engine::general_purpose, Engine as _};
+
+pub fn b64decode(encoded: &str) -> Vec<u8> {
+    general_purpose::STANDARD.decode(encoded).unwrap()
+}
+
+pub fn b64encode(s: &Vec<u8>) -> String {
+    general_purpose::STANDARD.encode(s)
+}
+
 use tao::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -8,6 +20,15 @@ use wry::WebViewBuilder;
 fn main() -> wry::Result<()> {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    // Read standard input
+    let mut input = Vec::new();
+    io::stdin()
+        .read_to_end(&mut input)
+        .expect("Failed to read from stdin");
+
+    // Base64 encode the input
+    let encoded_input = b64encode(&input);
 
     #[cfg(any(
         target_os = "windows",
@@ -30,13 +51,20 @@ fn main() -> wry::Result<()> {
         WebViewBuilder::new_gtk(vbox)
     };
 
-    let _webview = builder
+    let webview = builder
+        .with_devtools(true)
         .with_html(include_str!("../static/content.html"))?
         .with_ipc_handler(|message| {
             println!("Textarea content received: {}", message);
             std::process::exit(0);
         })
         .build()?;
+
+    // Inject JavaScript to decode the base64 string and set the textarea value
+    webview.evaluate_script(&format!(
+        "document.getElementById('content').value = b64ToUtf8('{}');",
+        encoded_input
+    ))?;
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
